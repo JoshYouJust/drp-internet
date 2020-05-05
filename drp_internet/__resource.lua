5ada6e103a900a4fddb3e450ca480eac26f47d5c
Sup6 <42062456+Sup66@users.noreply.github.com>
2020-05-05 14:01:56 +0300

    fOrGoT tHiS

---
+++ drp_internet/__resource.lua
@@ -0,0 +1,37 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af93'
+
+ui_page 'html/core.html'
+
+files {
+    --html stuff--
+    'html/core.html',
+    'html/core.css',
+    'html/core.js',
+    --Images--
+    --ads--
+    'html/img/ads/back.png',
+    'html/img/ads/exit.png',
+    'html/img/ads/forward.png',
+    'html/img/ads/bank.png',
+    'html/img/ads/dock.png',
+    'html/img/ads/Legendary.png',
+    'html/img/ads/LSMotorbikes.png',
+    'html/img/ads/NorthAuto.png',
+    'html/img/ads/OldieGoldie.png',
+    'html/img/ads/OnEarth.png',
+    'html/img/ads/SupAirways.png',
+    'html/img/ads/vip.png',
+    'html/img/ads/back.png',
+    --categories--
+    'html/img/categories/bank.png',
+    'html/img/categories/info.png',
+    'html/img/categories/media.png',
+    'html/img/categories/services.png',
+    'html/img/categories/shop.png',
+    'html/img/categories/travel.png',
+
+}
+
+client_scripts{
+    'client.lua',
+}
