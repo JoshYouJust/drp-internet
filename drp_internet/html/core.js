a5a695208f2fb670fa1fa78a6432ccbcd73d9835
Sup6 <42062456+Sup66@users.noreply.github.com>
2020-05-06 12:40:15 +0300

    Time for the flecca bank!

--- drp_internet/html/core.js
+++ drp_internet/html/core.js
@@ -5,6 +5,21 @@ function goBack(){
 function goForward(){
     window.history.forward();
 }
+
+function load(){
+    var elem = document.getElementById('bar');
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+      } else {
+        width++;
+        elem.style.width = width + '%';
+      }
+    }
+}
+
 $(document).ready(function(){
     function display(bool){
         if (bool===true) {
