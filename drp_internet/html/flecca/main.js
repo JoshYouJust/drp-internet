b0cd5882d1f75799f8e3a08ddfc49f7f3bdaee93
Sup6 <42062456+Sup66@users.noreply.github.com>
2020-05-06 23:36:22 +0300

    Make flecca great again!

---
+++ drp_internet/html/flecca/main.js
@@ -0,0 +1,14 @@
+function replace(div1,div2) {
+    document.getElementById(div1).style.display="none";
+    document.getElementById(div2).style.display="block";
+}
+function display(div1,div2,div3) {
+    document.getElementById(div1).style.display="block";
+    document.getElementById(div2).style.display="block";
+    document.getElementById(div3).style.display="block";
+}
+function hide(div1,div2,div3) {
+    document.getElementById(div1).style.display="none";
+    document.getElementById(div2).style.display="none";
+    document.getElementById(div3).style.display="none";
+}
