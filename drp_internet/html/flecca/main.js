7b603bc93b5522d570a61db7d2b1e3d4aaaac9fe
Sup6 <42062456+Sup66@users.noreply.github.com>
2020-05-07 13:41:39 +0300

    Almost Done! Ready for implementing.

--- drp_internet/html/flecca/main.js
+++ drp_internet/html/flecca/main.js
@@ -2,13 +2,15 @@ function replace(div1,div2) {
     document.getElementById(div1).style.display="none";
     document.getElementById(div2).style.display="block";
 }
-function display(div1,div2,div3) {
+function display(div1,div2,div3,div4) {
     document.getElementById(div1).style.display="block";
     document.getElementById(div2).style.display="block";
     document.getElementById(div3).style.display="block";
+    document.getElementById(div4).style.display="block";
 }
-function hide(div1,div2,div3) {
+function hide(div1,div2,div3,div4) {
     document.getElementById(div1).style.display="none";
     document.getElementById(div2).style.display="none";
     document.getElementById(div3).style.display="none";
+    document.getElementById(div4).style.display="none";
 }
